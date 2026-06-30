5e9a26cd65d4180be0053ee495f0e397b6140818
jv8gbf476b-ctrl <jv8gbf476b@privaterelay.appleid.com>
2026-06-30 23:51:43 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+BO_AI_5M
+config.py
+"""
+
+SYMBOL = "USDJPY=X"
+
+INTERVAL = "5m"
+PERIOD = "60d"
+
+THRESHOLD = 0.70
+
+TIMEZONE = "Asia/Tokyo"
+
+HISTORY_FILE = "history.csv"
+PENDING_FILE = "pending_signal.json"

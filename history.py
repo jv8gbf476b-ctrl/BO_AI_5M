77e169d3e8f82ce727367ecce711925e52f2445d
jv8gbf476b-ctrl <jv8gbf476b@privaterelay.appleid.com>
2026-06-30 23:53:12 +0900

    Create history.py

---
+++ history.py
@@ -0,0 +1,35 @@
+"""
+BO_AI_5M
+history.py
+"""
+
+import os
+import pandas as pd
+
+from config import HISTORY_FILE
+
+
+def load_history():
+
+    if not os.path.exists(HISTORY_FILE):
+        return pd.DataFrame()
+
+    return pd.read_csv(HISTORY_FILE)
+
+
+def append_history(record):
+
+    df = load_history()
+
+    df = pd.concat(
+        [
+            df,
+            pd.DataFrame([record]),
+        ],
+        ignore_index=True,
+    )
+
+    df.to_csv(
+        HISTORY_FILE,
+        index=False,
+    )

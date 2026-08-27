6bdd8a8eecd02e5d13aa7dfd0ab70b6b1675a6a2
jv8gbf476b-ctrl <jv8gbf476b@privaterelay.appleid.com>
2026-08-27 13:36:53 +0900

    Update run_lab.py

--- research/run_lab.py
+++ research/run_lab.py
@@ -6,7 +6,26 @@ run_lab.py
 研究所単独テスト
 """
 
-from confidence_lab import (
+import os
+import sys
+
+
+# BO_AI_5M のルートフォルダを
+# Pythonの読み込み対象に追加
+ROOT_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+
+if ROOT_DIR not in sys.path:
+    sys.path.insert(
+        0,
+        ROOT_DIR,
+    )
+
+
+from research.confidence_lab import (
     make_confidence_report,
 )
 
